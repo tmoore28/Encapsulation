27f7b7b00e7c867dcf0a081f76f77afeacc3b6ec
Tylor Moore <mooretylor28@gmail.com>
2022-03-31 16:58:17 -0400

    initial commit

---
+++ BankEncapsulation/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var yourAccount = new BankAccount();
+            Console.WriteLine("Current Balance");
+            Console.WriteLine(yourAccount.GetBalance());
+
+            yourAccount.Deposit(124.75);
+            Console.WriteLine("Current Balance");
+            Console.WriteLine(yourAccount.GetBalance());
+            Console.ReadKey();
+        }
+    }
+}
